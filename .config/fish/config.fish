61c4931a7b1eec4b9c5ecd8067a1b84fe171877c
rodzera <rodnascimentolopes@gmail.com>
2025-04-09 11:38:49 -0300

    update: config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,13 @@
 set fish_prompt_pwd_dir_length 0
 starship init fish | source
+set -x ANDROID_HOME $HOME/Android/Sdk
+set -x PATH $PATH $ANDROID_HOME/emulator
+set -x PATH $PATH $ANDROID_HOME/platform-tools
+if status is-interactive
+    and not set -q TMUX
+    if set -q TERMINAL_EMULATOR
+        tmux new-session -As jediterm
+    else
+        tmux new-session -As gnome
+    end
+end
