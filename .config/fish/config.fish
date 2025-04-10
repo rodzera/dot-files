03a8217e6b2a2cc6cb4664f451070746258d801c
rodzera <rodnascimentolopes@gmail.com>
2025-04-10 01:46:15 -0300

    fix: fish session management

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,22 @@
 set fish_prompt_pwd_dir_length 0
 starship init fish | source
+
 set -x ANDROID_HOME $HOME/Android/Sdk
 set -x PATH $PATH $ANDROID_HOME/emulator
 set -x PATH $PATH $ANDROID_HOME/platform-tools
+
 if status is-interactive
     and not set -q TMUX
-    set session (set -q TERMINAL_EMULATOR; and echo jediterm; or echo gnome)
+    
+    # kill all detached sessions
+    tmux list-sessions -F "#{session_name}:#{session_attached}" | \
+        grep ':0$' | \
+        grep -v "^$uuid:" | \
+        cut -d: -f1 | \
+        xargs -r -n1 tmux kill-session -t
 
-    tmux has-session -t $session 2>/dev/null
-    or tmux new-session -ds $session \; source-file ~/.tmux.conf
-
-    tmux attach-session -t $session
+    # create and attach a unique session
+    set uuid (uuidgen)
+    tmux new-session -d -s $uuid \; source-file ~/.tmux.conf
+    tmux attach-session -t $uuid
 end
